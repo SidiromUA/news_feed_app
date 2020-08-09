fa7d9538b5c2423b169b911f797e69dd810263a7
Roman Golovatyi <roman@golovatiy.com>
2020-08-09 22:45:03 +0300

    Added contain images

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :find_article, only: [:show, :edit, :update, :destroy, :create]
 
   def index
     @articles = Article.all
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
   def edit;end
 
   def update
-    if @article.update_attributes(article_params)
+    if @article.update(article_params)
       redirect_to @article, success: 'Article successfully updated'
     else
       flash[:danger] = 'Article not updated'
@@ -44,6 +44,6 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :content, :published, :private)
+    params.require(:article).permit(:title, :content, :published, :private, :image)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
+  mount_uploader :image, ImageUploader
   validates :title, :content, presence: true
 end
